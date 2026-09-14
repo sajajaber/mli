331ac99a6dd9cb644ceb674fb3b564af1e40ee53
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-14 20:33:50 +0300

    Rename homepage site content reference

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.public :title="'Media Link International'">
     <x-partials.hero :shows="$shows" />
     <x-partials.content-teaser :shows="$shows" />
-    <x-partials.about-section :page="$aboutPage" />
+    <x-partials.about-section :content="$aboutPage" />
     <x-partials.news-section :media-news="$mediaNews" :mli-news="$mliNews" />
 
     {{-- Clients, Contact sections coming next --}}
