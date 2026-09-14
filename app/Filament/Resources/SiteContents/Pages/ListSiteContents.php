886bd23907db2a647ca06362474e3afb74011bfd
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-14 20:47:21 +0300

    Remove Site Content creation action

--- app/Filament/Resources/SiteContents/Pages/ListSiteContents.php
+++ app/Filament/Resources/SiteContents/Pages/ListSiteContents.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\SiteContents\Pages;
 
 use App\Filament\Resources\SiteContents\SiteContentResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSiteContents extends ListRecords
 {
     protected static string $resource = SiteContentResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
