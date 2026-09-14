95c2247401aba96393974a43f889ba7f8aa4e530
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-14 20:33:17 +0300

    Add Site Content create page

---
+++ app/Filament/Resources/SiteContents/Pages/CreateSiteContent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SiteContents\Pages;
+
+use App\Filament\Resources\SiteContents\SiteContentResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSiteContent extends CreateRecord
+{
+    protected static string $resource = SiteContentResource::class;
+}
