d224c9870502f56a28fc89cfed518f696b86bc65
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-22 16:02:19 +0300

    Undo duplicate image detection

--- app/Filament/Resources/People/PersonResource.php
+++ app/Filament/Resources/People/PersonResource.php
@@ -37,7 +37,7 @@ class PersonResource extends Resource
                 ->imageEditor()
                 ->maxSize(8192)
                 ->saveUploadedFileUsing(fn($file) => app(\App\Services\ImageProcessingService::class)
-                    ->processAndStore($file, 'people', maxWidth: 800, quality: 85, field: 'photo_path'))
+                    ->processAndStore($file, 'people', maxWidth: 800, quality: 85))
                 ->columnSpanFull(),
 
             TextInput::make('name')
