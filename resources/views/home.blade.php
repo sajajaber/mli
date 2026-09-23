d41605f77079aaf0c9b5443a54e372174382d6be
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-23 19:45:09 +0300

    Remove separate homepage New Releases section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,7 @@
 <x-layouts.public :title="'Media Link International'">
     <x-partials.hero :shows="$shows" />
     <x-partials.content-teaser :shows="$shows" />
-    <x-partials.new-releases :releases="$newReleases" />
     <x-partials.about-section :content="$aboutPage" :stats="$aboutStats" :services="$mediaServices" />
     <x-partials.key-people :people="$people" />
     <x-partials.news-section :media-news="$mediaNews" :mli-news="$mliNews" />
-
-
 </x-layouts.public>
