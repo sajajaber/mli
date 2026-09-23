23ec593a194303d8f25b5bd0500ff3f3653483c9
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-23 19:00:16 +0300

    Pass media services to About section

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -77,6 +77,7 @@ class HomeController extends Controller
             'mliNews' => $mliNews,
             'aboutPage' => $aboutPage,
             'aboutStats' => $aboutStats,
+            'mediaServices' => $mediaServices,
         ]);
     }
 }
