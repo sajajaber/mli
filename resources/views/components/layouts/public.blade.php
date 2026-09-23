fa3bb54d858831a5d5b42bbed46c1d1b88a8c5fa
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-23 21:45:13 +0300

    Add Arabic display font

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -10,7 +10,7 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;500;600;700&family=Readex+Pro:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
