fc5c3e2ae74ee9adb3f225310395e7973c5d1f44
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-23 20:22:17 +0300

    Redirect after creating news to news list

--- app/Filament/Resources/News/Pages/CreateNews.php
+++ app/Filament/Resources/News/Pages/CreateNews.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateNews extends CreateRecord
 {
     protected static string $resource = NewsResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return NewsResource::getUrl('index');
+    }
 }
