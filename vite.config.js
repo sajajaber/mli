58226e76b9d293c5c0f064dbba35f82d37a7d0e8
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-23 21:52:22 +0300

    fix: remove missing logo entry from Vite inputs

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/css/filament/admin/theme.css",
-                "resources/assets/images/mli-logo.png",
             ],
             refresh: true,
         }),
