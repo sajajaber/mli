f1bb359f536ed2af494478d87d93d2b62820869c
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-23 21:49:37 +0300

    Add footer settings list page

---
+++ app/Filament/Resources/FooterSettings/Pages/ListFooterSettings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FooterSettings\Pages;
+
+use App\Filament\Resources\FooterSettings\FooterSettingResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListFooterSettings extends ListRecords
+{
+    protected static string $resource = FooterSettingResource::class;
+}
