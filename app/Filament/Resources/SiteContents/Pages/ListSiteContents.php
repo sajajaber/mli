7e023d64373b4decee3b8f072a825a3f5f08fec9
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-23 22:10:22 +0300

    feat: show footer settings on site content page

--- app/Filament/Resources/SiteContents/Pages/ListSiteContents.php
+++ app/Filament/Resources/SiteContents/Pages/ListSiteContents.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\SiteContents\Pages;
 
 use App\Filament\Resources\SiteContents\SiteContentResource;
+use App\Filament\Resources\SiteContents\Widgets\FooterSettingsRow;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSiteContents extends ListRecords
 {
     protected static string $resource = SiteContentResource::class;
+
+    protected function getFooterWidgets(): array
+    {
+        return [
+            FooterSettingsRow::class,
+        ];
+    }
 }
