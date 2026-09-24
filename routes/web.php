e2021e0b88192fbf7c0742d6657548baf409d522
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-24 16:55:47 +0300

    Add public News routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShowController;
+use App\Http\Controllers\NewsController;
 
 Route::get('/shows', [ShowController::class, 'index'])->name('shows.index');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/news/{slug}', [NewsController::class, 'show'])->name('news.show');
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
