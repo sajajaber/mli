419e7a9108f4651409bab3a63944bdf13312138d
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-24 16:09:29 +0300

    Match shows page navbar and typography context

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -15,7 +15,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body class="{{ request()->routeIs('shows.index') ? 'shows-page-body' : '' }}">
     @include('partials.nav')
     <main>{{ $slot }}</main>
     @include('partials.footer')
